c01f28af9851cf002951720496370d89608fc7e9
mihay42 <mihay42@gmail.com>
2019-05-12 16:56:51 -0700

    Updated to include master.idx instead of company.idx.  Emphasizing the 10-k filings as a starting point.

--- parse_idx.py
+++ parse_idx.py
@@ -1,25 +1,37 @@
 import re
+import pprint
 
-f1 = open("company.20190401.idx")
+f1 = open("master.idx")
 
-# TODO separate the header from the body of the idx, remove the '----' separator in appropriate regex instances
-header_re = re.compile('\w+\s*:\s*\w+\s*', re.IGNORECASE)
-ignore_re = re.compile('^-.*$')
+header_re = re.compile('^\w+.*:', re.IGNORECASE)  # Detect the headers for the idx
+ignore_re = re.compile('^-.*$')  # Skip the separator which is '----'
 idx = {}
 raw_data = []
 for line in f1.readlines():
-    line.strip()
-    if header_re.match(line):
-        (key, value) = line.split(':')
-        idx[key] = value.strip()
+    line = line.rstrip()
+    if line:
+        # Used to generate the header of the file specific index
+        if header_re.match(line):
+            (key, value) = line.split(':', 1)
+            idx[key] = value
+            continue
+        elif ignore_re.match(line):
+            continue
+        entry = {}
+        (cik, company, form, date, fil) = line.split('|')
+        if form == '10-K':
+            entry = {
+                'CIK': cik,
+                'Company Name': company,
+                'Form Type': form,
+                'Date Filed': date,
+                'File Name': fil
+            }
+            raw_data.append(entry)
+        else:
+            continue
+    else:
         continue
-    elif ignore_re.match(line):
-        continue
 
-# TODO stuff a dict with appropriate company data into an array
-
-    #line_dict=dict(line.split())
-    #raw_data.append(line_dict)
-
-#print (raw_data)
-print (idx)
+idx['payload'] = raw_data
+pprint.pprint(idx)
