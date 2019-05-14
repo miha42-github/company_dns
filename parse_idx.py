235f385098bf8a8ad8b16134ee2da2d894000ad6
mihay42 <mihay42@gmail.com>
2019-05-13 20:57:11 -0700

    Enabled searching based upon company name in the function. Cleaned up a bug which was incorrectly matching company entries

--- parse_idx.py
+++ parse_idx.py
@@ -1,37 +1,54 @@
 import re
-import pprint
+from pprint import pprint as printer
 
-f1 = open("master.idx")
 
-header_re = re.compile('^\w+.*:', re.IGNORECASE)  # Detect the headers for the idx
-ignore_re = re.compile('^-.*$')  # Skip the separator which is '----'
-idx = {}
-raw_data = []
-for line in f1.readlines():
-    line = line.rstrip()
-    if line:
-        # Used to generate the header of the file specific index
-        if header_re.match(line):
-            (key, value) = line.split(':', 1)
-            idx[key] = value
-            continue
-        elif ignore_re.match(line):
-            continue
-        entry = {}
-        (cik, company, form, date, fil) = line.split('|')
-        if form == '10-K':
-            entry = {
-                'CIK': cik,
-                'Company Name': company,
-                'Form Type': form,
-                'Date Filed': date,
-                'File Name': fil
-            }
-            raw_data.append(entry)
+def build_idx(file_name, company_name=None, report_type="10-K"):
+    global entry
+    header_re = re.compile('^\w+.*:', re.IGNORECASE)  # Detect the headers for the idx
+    ignore_re = re.compile('^-.*$')  # Skip the separator which is '----'
+    skip_re = re.compile('^\S+\|')  # Skip an entry if it has a | symbol
+    idx = {}
+    raw_data = []
+    for line in file_name.readlines():
+        line = line.rstrip()
+        line = line.strip()
+        if line:
+            # Used to generate the header of the file specific index
+            if header_re.match(line) and not skip_re.match(line):
+                (key, value) = line.split(':', 1)
+                idx[key] = value
+                continue
+            elif ignore_re.match(line):
+                continue
+            (cik, company, form, date, fil) = line.split('|')
+            if form == report_type:   # Filter in only the report types we want to see, default is 10-k only
+                if company_name.lower() in company.lower():  # TODO test this scenario including substrings
+                    entry = {
+                        'CIK': cik,
+                        'Company Name': company,
+                        'Form Type': form,
+                        'Date Filed': date,
+                        'File Name': fil
+                    }
+                    raw_data.append(entry)
+                elif company is None:
+                    entry = {
+                        'CIK': cik,
+                        'Company Name': company,
+                        'Form Type': form,
+                        'Date Filed': date,
+                        'File Name': fil
+                    }
+                    raw_data.append(entry)
+            else:
+                continue
         else:
             continue
-    else:
-        continue
 
-idx['payload'] = raw_data
-pprint.pprint(idx)
+    idx['payload'] = raw_data
+    return idx
+
+
+f1 = open("master.idx")
+index = build_idx(f1, "BAR")
+printer(index)
