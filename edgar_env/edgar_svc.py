a667da7c2cae4b0da3eb3931b3173692064a6d3a
mihay42 <mihay42@gmail.com>
2019-10-17 21:33:08 -0700

    Updated the db structure to better represent the target directory

--- edgar_env/edgar_svc.py
+++ edgar_env/edgar_svc.py
@@ -1,6 +1,7 @@
 #!edgar/bin/python
 
 """Core RESTful service to retrieve EDGAR information about companies."""
+from typing import List, Any
 
 from flask import Flask, jsonify, abort, make_response
 from flask_httpauth import HTTPBasicAuth
@@ -10,6 +11,7 @@ from pyedgar.filing import Filing
 from pyedgar.index import EDGARIndex
 from os import path
 import os.path
+import re
 
 # Setup the application name and basic operations
 app = Flask(__name__, static_url_path="")
@@ -31,7 +33,7 @@ def unauthorized():
     return make_response(jsonify({'message': 'Unauthorized'}), 403)
 
 
-def initialize(my_file='.db_present', my_year=2017):
+def _initialize(my_file='.db_present', my_year=2017):
     """
     If the EDGAR index isn't available download the raw files and create the index.
     This is controlled by "my_file" which is created at the time of the initial
@@ -53,14 +55,94 @@ def initialize(my_file='.db_present', my_year=2017):
         return True
 
 
+def _get10ks() -> object:
+    """
+    Gather all of the various indexes available, filter down to only the 10-Ks,
+    and return only a 10-K index.  This prepares for searching these metadata and
+    getting the header information.
+
+    :rtype: object
+    """
+    all_idxs = EDGARIndex()  # Instantiate the EDGAR indexes
+    all_10ks = all_idxs['10-K']  # Pull in only the 10-K portion of the indexes
+    # This is likely not needed: all_10ks.drop(['form', 'filedate'], axis=1)
+    return all_10ks
+
+
+companies_fields = {
+    'name': fields.String,
+    'headers': fields.Arbitrary,
+    'abstract': fields.String,
+    'done': fields.Boolean,
+    'uri': fields.Url('filings')
+}
+
+
 class edgarFilingAPI(Resource):
     decorators = [auth.login_required]
 
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
+        self.index = _get10ks()
+        self.reqparse.add_argument('name', required=True, help="No company name provided",
+                                   location="json")
+        super(edgarFilingAPI, self).__init__()
 
-    def get(self):
-        pass
+    def get(self, company):
+        to_skip = re.compile('company-conformed-name|state-of-incorporation|street1|zip|state|'
+                             'standard-industrial-classification|form-type|fiscal-year-end|irs-number|filer|'
+                             'city|business-phone|sec-act|sec-file-number|\<sec-document|\<sec-header')
+        to_mod = re.compile('accession-number')
+        companies_list: List[Any] = []
+        companies_dict: Dict[str, Union[Dict[Any, Any], Dict[str, Dict[Any, Any]]]] = dict()
+
+        for key, value in self.index['name'].str.contains(company).items():
+            if value:
+                companies_list.append(self.index.loc[[key]].values.tolist())
+
+        for entry in companies_list:
+            n = entry[0]
+            cik = n[0]
+            name = n[1]
+            ascension = n[4]
+            f = Filing(cik, ascension)
+
+            my_cik = str(f.headers['central-index-key']).strip()
+            my_report = str(f.headers['conformed-period-of-report']).strip()
+            if my_cik in companies_dict:
+                companies_dict[my_cik].update({my_report: dict()})
+            else:
+                companies_dict[my_cik] = dict()
+                companies_dict[my_cik] = {my_report: dict(),
+                                     'company_name': str(f.headers['company-conformed-name']).strip().lstrip("'"),
+                                     'country': 'United States of America',
+                                     'state': str(f.headers['state']).strip(),
+                                     'city': str(f.headers['city']).strip(),
+                                     'street': str(f.headers['street1']).strip(),
+                                     'zip_code': str(f.headers['zip']).strip(),
+                                     'business_phone': '+1' + str(f.headers['business-phone']).strip(),
+                                     'state_of_incorporation': str(f.headers['state-of-incorporation']).strip(),
+                                     'industry': str(f.headers['standard-industrial-classification']).strip(),
+                                     'form_type': str(f.headers['form-type']).strip(),
+                                     'fiscal_end': str(f.headers['fiscal-year-end']).strip(),
+                                     'irs_number': str(f.headers['irs-number']).strip(),
+                                     'sec_file_number': str(f.headers['sec-file-number']).strip(),
+                                     }
+
+            for item in f.headers:
+                if to_skip.match(item):
+                    continue
+                elif to_mod.match(item):
+                    directory = my_cik + '/' + str(f.headers[item].replace('-', ''))
+                    companies_dict[my_cik][my_report].update({'url': f.urls[1]})
+                    root_url = EDGARURI + EDGARSERVER + '/' + EDGARARCHIVES + '/' + directory + '/'
+                    companies_dict[my_cik][my_report].update({'directory_url': root_url})
+
+                companies_dict[my_cik][my_report].update({item: f.headers[item]})
+
+            if len(companies_dict) == 0:
+                abort(404)
+            return {'headers': marshal(companies_dict, companies_fields)}
 
 
 class edgar10KAbstractAPI(Resource):
@@ -77,5 +159,5 @@ api.add_resource(edgarFilingAPI, '/edgar/api/v1.0/filings', endpoint="filings")
 api.add_resource(edgar10KAbstractAPI, '/edgar/api/v1.0/abstracts', endpoint="abstract")
 
 if __name__ == '__main__':
-    initialize()
+    _initialize()
     app.run(debug=True)
