e62088afb8a73a96e841d8272febd4634f4c7bc4
mihay42 <mihay42@gmail.com>
2019-11-03 20:59:58 -0800

    Updated to focus on form type structure and added URLs.

--- edgar_env/edgar/bin/falsh.py
+++ edgar_env/edgar/bin/falsh.py
@@ -14,8 +14,11 @@
 
 import sqlite3
 import cmd
+import re
+import requests
 from pprint import pprint as printer
 from pyedgar.filing import Filing
+from bs4 import BeautifulSoup
 
 __author__ = "Michael Hay"
 __copyright__ = "Copyright 2019, Cameron Solutions"
@@ -25,6 +28,7 @@ __maintainer__ = "Michael Hay"
 __status__ = "Prototype"
 __id__ = "$Id$"
 
+# Constants
 PATH = "./"
 DB_CACHE = 'edgar_cache.db'
 COMPANY = 1
@@ -34,15 +38,10 @@ MONTH = 3
 DAY = 4
 ACCESSION = 5
 FORM = 6
+EDGARSERVER = "www.sec.gov"
+EDGARARCHIVES = "Archives/edgar/data"
+EDGARURI = "https://"
 
-# TODO make this a simple CLI allowing for searching based upon company name without needing SQL
-# TODO surface the 10K HTML URLs
-# TODO pull in PyEdgar to print out the header information
-# TODO pull in PyEdgar to print out a company description from an 10K or similar
-# TODO try a query to something other than a 10k and this implies getting the form type in the db cache
-# COMMENT The structure above should be related to the process of picking the company name and then from there
-# gathering more information such as address, phone, industry, etc.  This would suggest that there are two
-# other RESTful methods: get_headers and get_abstract -> from a 10k or 10q
 
 e_conn = sqlite3.connect(PATH + DB_CACHE)
 ec = e_conn.cursor()
@@ -51,39 +50,61 @@ ec = e_conn.cursor()
 class FalshCmd(cmd.Cmd):
 
     prompt = 'falsh> '
-    intro = 'A shell to retrieve firmalitics from various databases like SEC Edgar'
+    intro = 'A shell to retrieve firmalitics from the SEC Edgar related to public companies'
     ruler = '_'
 
     def do_cquery(self, query):
         companies = {}
-        # TODO Consider filtering in only 10-Ks, S1s, etc. this would use regex to match
+        white_list = re.compile('10-K|10-Q')
         for row in ec.execute("SELECT * FROM companies WHERE name LIKE '%" + query + "%'"):
-            my_row = [row[MONTH], row[DAY], row[FORM], row[ACCESSION]]
+            if not white_list.match(row[FORM]):
+                continue
+            l_dir = str(row[CIK]) + '/' + row[ACCESSION].replace('-', '')
+            doc_url = EDGARURI + EDGARSERVER + '/' + EDGARARCHIVES + '/' + l_dir + '/'
+            l_file = doc_url + row[ACCESSION] + '-index.html'
+            my_row = [row[MONTH], row[DAY], row[YEAR], row[ACCESSION], doc_url, l_file]
             if row[CIK] in companies:
-                if str(row[YEAR]) in companies[row[CIK]]:
-                    companies[row[CIK]][str(row[YEAR])].append(my_row)
+                if str(row[FORM]) in companies[row[CIK]]:
+                    companies[row[CIK]][row[FORM]].append(my_row)
                 else:
-                    companies[row[CIK]][str(row[YEAR])] = []
-                    companies[row[CIK]][str(row[YEAR])].append(my_row)
+                    companies[row[CIK]][row[FORM]] = []
+                    companies[row[CIK]][row[FORM]].append(my_row)
             else:
-                companies[row[CIK]]={'name': row[COMPANY], str(row[YEAR]): []}
-                companies[row[CIK]][str(row[YEAR])].append(my_row)
+                companies[row[CIK]]={'name': row[COMPANY], row[FORM]: []}
+                companies[row[CIK]][row[FORM]].append(my_row)
         printer(companies)
 
     def help_cquery(self):
         print("\ncquery [company name OR string]\nQuery either a company name or partial name.")
 
-    def do_cikquery(self, query):
-        for row in ec.execute("SELECT * FROM companies WHERE cik is '" + query + "'"):
-            printer(row[1] + '(CIK: ' + str(row[0]) + "): " + row[5])
-
     def do_getheaders(self, cik_accession):
+        accession: str
         (cik, accession) = cik_accession.split(':')
-        f = Filing (cik, accession)
+        f = Filing(cik, accession)
         printer(f.headers)
 
-    def do_getfiledesc(self, query):
-        pass
+    # TODO pull in PyEdgar to print out a company description from an 10K or similar
+    # TODO we may need to parse from item 1. to item 1.a in a case insensitive form.  Merely getting all HTML could be ok
+    def do_getdesc(self, l_url):
+        page = requests.get(l_url)
+        item_1 = re.compile('item 1.', re.IGNORECASE)
+        item_1a = re.compile('item 1a.', re.IGNORECASE)
+        html = page.content
+        l_string = ""
+        flag = 0
+        for l in html:
+            line = str(l)
+            if item_1.match(line):
+                flag = 1
+            elif item_1a.match(line):
+                flag = 0
+            elif flag == 1:
+                l_string += line
+            else:
+                continue
+        print(html)
+
+
 
     def emptyline(self):
         pass
